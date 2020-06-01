db05fb3719323a3a3533d37206c4ba6b76ab5b92
yongjie <yong-jie.gong@microfocus.com>
2020-06-01 11:09:31 +0800

    add graavlvm

---
+++ basic-parent/basic-jdk/basic-graavlvm/hello/src/test/java/org/basic/graavlvm/quarkus/NativeGreetingIT.java
@@ -0,0 +1,9 @@
+package org.basic.graavlvm.quarkus;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingIT extends GreetingTest {
+
+    // Execute the same tests but in native mode.
+}
